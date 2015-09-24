34908131658f9578df6fdad85fb8e59f69d01f46
Hayden Scott <hascott@mymail.mines.edu>
2015-09-24 11:57:28 -0600

    initial commmit

---
+++ src/ScoreTrakker.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class ScoreTrakker {
+	ArrayList<Student> students = new ArrayList();
+	
+	public void loadDataFromFile (String s) {
+		
+	}
+	
+	public void printInOrder () {
+		
+	}
+	
+	public void processFiles() {
+		loadDataFromFile("scores.txt");
+		printInOrder();
+	}
+	
+	public static void main(String [ ] args) {
+		
+	}
+}
